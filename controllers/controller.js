09240ef13adb9ded777bbf8c3bdb14052cdbdf3f
Chelsey Eichler <chelsey.eichler@yahoo.com>
2017-03-23 18:32:09 -0400

    added style and cortney's changes

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,7 @@ var app = angular.module('cheapDate');
 
 
 app.controller('cuisCtrl',function($scope, dateFactory){
+  $scope.listItem = '';
   $scope.getlist = function(){
   console.log('hey');
   dateFactory.dates().then(function successCallback(response) {
