7f7fc4352ebca7fe760afba9a7b30bcef9f765e2
Chelsey Eichler <chelsey.eichler@yahoo.com>
2017-03-30 20:55:02 -0400

    tinkering with jump to

--- script.js
+++ script.js
@@ -2,17 +2,5 @@ $(document).ready( function(){
 
 
 
-    $('#inner-content-div').slimScroll({
-        height: '50px'
-    });
-
-
-// $('#slimtest2').slimScroll({
-//     position: 'left',
-//     height: '150px',
-//     railVisible: true,
-//     alwaysVisible: true
-// });
-
 
 } );
